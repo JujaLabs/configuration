3a6b3fcd5687f57bc948e99b3cbd82d8f6b8e014
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-11-20 22:36:13 +0200

    add new version users service

--- config/dev/x2crmdummy/create_x2contacts_table.sql
+++ config/dev/x2crmdummy/create_x2contacts_table.sql
@@ -9,12 +9,14 @@ CREATE TABLE IF NOT EXISTS x2crm.x2_contacts (
     c_slack varchar(255) DEFAULT NULL,
     c_isStudent tinyint(1) DEFAULT 0,
     c_uuid varchar(255) DEFAULT NULL,
+    assignedTo varchar(50) DEFAULT NULL,
+    visibility  int(11) DEFAULT NULL,
     PRIMARY KEY(id)
 );
 
 
-INSERT INTO x2crm.x2_contacts (name,firstName,lastName,skype,lastUpdated,c_gmail,c_slack,c_isStudent,c_uuid) VALUES('Alex','Alex','BatmanNew','Alex',50,'alex@gmail.com','@batman',1,'00000000-0000-0001-0000-000000000002');
-INSERT INTO x2crm.x2_contacts (name,firstName,lastName,skype,lastUpdated,c_gmail,c_slack,c_isStudent,c_uuid) VALUES('Max','Max','Ironman','max.ironman',200,'max@gmail.com','@max',1,'00000000-0000-0001-0000-000000000003');
-INSERT INTO x2crm.x2_contacts (name,firstName,lastName,skype,lastUpdated,c_gmail,c_slack,c_isStudent,c_uuid) VALUES('Sergey','SergeyUpdate','Spiderman','sergey.spiderman',10,'sergey@gmail.com','@spiderman',1,'00000000-0000-0001-0000-000000000004');
-INSERT INTO x2crm.x2_contacts (name,firstName,lastName,skype,lastUpdated,c_gmail,c_slack,c_isStudent,c_uuid) VALUES('Apex','Sergey','Apex','apea.spiderman',1000,'apex@gmail.com','@apex',1,'00000000-0000-0001-0000-000000000005');
-INSERT INTO x2crm.x2_contacts (name,firstName,lastName,skype,lastUpdated,c_gmail,c_slack,c_isStudent,c_uuid) VALUES('NewUserWithOutUpdateName','NewUserWithOutUpdateFirstName','NewUserWithOutUpdateLastName','NewUserWithOutUpdateSkype',500,'NewUserWithOutUpdateGmail@gmail.com','@NewUserWithOutUpdateSlack',1,'00000000-0000-0001-0000-0000000000010');
+INSERT INTO x2crm.x2_contacts (name,firstName,lastName,skype,lastUpdated,c_gmail,c_slack,c_isStudent,c_uuid, assignedTo, visibility) VALUES('Alex','Alex','BatmanNew','Alex',50,'alex@gmail.com','@batman',1,'00000000-0000-0001-0000-000000000002','Anyone',1);
+INSERT INTO x2crm.x2_contacts (name,firstName,lastName,skype,lastUpdated,c_gmail,c_slack,c_isStudent,c_uuid, assignedTo, visibility) VALUES('Max','Max','Ironman','max.ironman',200,'max@gmail.com','@max',1,'00000000-0000-0001-0000-000000000003','Anyone',1);
+INSERT INTO x2crm.x2_contacts (name,firstName,lastName,skype,lastUpdated,c_gmail,c_slack,c_isStudent,c_uuid, assignedTo, visibility) VALUES('Sergey','SergeyUpdate','Spiderman','sergey.spiderman',10,'sergey@gmail.com','@spiderman',1,'00000000-0000-0001-0000-000000000004','Anyone',1);
+INSERT INTO x2crm.x2_contacts (name,firstName,lastName,skype,lastUpdated,c_gmail,c_slack,c_isStudent,c_uuid, assignedTo, visibility) VALUES('Apex','Sergey','Apex','apea.spiderman',1000,'apex@gmail.com','@apex',1,'00000000-0000-0001-0000-000000000005','Anyone',1);
+INSERT INTO x2crm.x2_contacts (name,firstName,lastName,skype,lastUpdated,c_gmail,c_slack,c_isStudent,c_uuid, assignedTo, visibility) VALUES('NewUserWithOutUpdateName','NewUserWithOutUpdateFirstName','NewUserWithOutUpdateLastName','NewUserWithOutUpdateSkype',500,'NewUserWithOutUpdateGmail@gmail.com','@NewUserWithOutUpdateSlack',1,'00000000-0000-0001-0000-0000000000010','Anyone',1);
